763e5e82e881c550983280df72574e9252992972
davide136 <davide136@gmail.com>
2020-07-31 09:16:01 +0200

    corrected to enable installation of stuff

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -33,6 +33,7 @@ $tweaks = @(
 	"InstallTitusProgs", #REQUIRED FOR OTHER PROGRAM INSTALLS!
 	"InstallAdobe",
 	"Install7Zip",
+	"InstallJava",
 #	"InstallNotepadplusplus",
 #	"InstallMediaPlayerClassic",
 
